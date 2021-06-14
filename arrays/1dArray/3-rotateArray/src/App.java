0472b328e9f3aa4975a1322107ea56c1098624d5
Abhishek Kandi <kandii.abhishek@gmail.com>
2021-06-14 18:12:53 +0530

    Rotating array k times

--- arrays/1dArray/3-rotateArray/src/App.java
+++ arrays/1dArray/3-rotateArray/src/App.java
@@ -1,25 +1,42 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int[] orginalArray = new int[] { 1,2,3,4 };
-        
-        int k = 199;
-        k = k % orginalArray.length;
-        for(int i = 1; i <=k; i++)
-            orginalArray = rotateArrayRightOnce(orginalArray);
+        orginalArray = rotateArrayRightIndexAlgorithm(orginalArray, 200);
         printArray(orginalArray);
     }
 
-    private static int[] rotateArrayRightOnce(int[] array){
-        int[] result = new int[array.length];
-
-        int temp = array[array.length - 1];
-        for(int i = 0; i < array.length - 1; i++)
-            result[i+1] = array[i];
+    /*  
+        For an Index i of an Array A, Upon rotating K times 
+        it's new position will be (i + K) % A.length
+        */
+    private static int[] rotateArrayRightIndexAlgorithm(int[] originalArray, int kTimes){
         
-        result[0] = temp;
+        int sizeOfArray = originalArray.length;
+        /* 
+        Once the array is rotated by it's size times, 
+        all the elements will go back to it's original postition
+        */
+        kTimes = kTimes % sizeOfArray; 
+        int[] rotatedArray = new int[sizeOfArray];
+        for(int i = 0; i < sizeOfArray; i++){
+            int newPosition = (i + kTimes) % sizeOfArray;
+            rotatedArray[newPosition] =originalArray[i];
+        }       
 
-        return result;
+        return rotatedArray;
     }
+
+    // private static int[] rotateArrayRightOnce(int[] array){
+    //     int[] result = new int[array.length];
+
+    //     int temp = array[array.length - 1];
+    //     for(int i = 0; i < array.length - 1; i++)
+    //         result[i+1] = array[i];
+        
+    //     result[0] = temp;
+
+    //     return result;
+    // }
 
     private static void printArray(int[] array){
         for(int i = 0; i < array.length; i++)   
