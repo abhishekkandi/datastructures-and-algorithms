d8899e3fc5f62dfa3757f55b5a34cf28a925a092
Abhishek Kandi <kandii.abhishek@gmail.com>
2021-06-16 14:04:11 +0530

    Optimized Solution for Different Bit Sum pairwise

--- bit-manipulation/2-Different Bits Sum Pairwise/src/App.java
+++ bit-manipulation/2-Different Bits Sum Pairwise/src/App.java
@@ -3,6 +3,32 @@ public class App {
         System.out.println(cntBits(new int[] { 1,3,5 }));
     }
 
+    /*     
+    Some background : 
+        If X = 0, Y = 0 Then X (XOR/^) Y = 0
+        If X = 1, Y = 0 Then X ^ Y = 1
+        XOR will onl set
+
+    Algorithm: 
+        Calculate the number of set bits and unset bits at all 32 bits for all elements
+    */
+    public static long cntBits(int[] A){
+
+        long bitSum = 0;
+        for(int i = 0; i < 31; i++){
+            int setBitsAtI = 0;
+            for(int elementIndex = 0; elementIndex < A.length; elementIndex++){
+                if(((1 << i) & A[elementIndex]) > 0)
+                    setBitsAtI+=1;
+            }
+            bitSum += (2L * setBitsAtI * (A.length - setBitsAtI));// Because XOR will set bit only if X and Y are different
+        }
+        return bitSum % ((long)Math.pow(10,9) + 7);
+    }
+
+    /*
+    TC for below algo: O(N^2)
+
     public static int cntBits(int[] A) {
         //Time complexity : 
         int arraySize = A.length, bitSum = 0;
@@ -28,4 +54,5 @@ public class App {
 
         return cntSetBits;
     }
+    */
 }
