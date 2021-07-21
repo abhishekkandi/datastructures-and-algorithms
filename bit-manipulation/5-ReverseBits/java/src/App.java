daa89b274ddbbbe422e695915afd2dc5f20d588f
Abhishek Kandi <kandii.abhishek@gmail.com>
2021-07-21 17:58:53 +0530

    Reverse bits incomplete

---
+++ bit-manipulation/5-ReverseBits/java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    public long reverse(long a) {
+	    return 0;
+	}
+}
